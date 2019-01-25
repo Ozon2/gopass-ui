27fc8b4efa1bdbca2d1cfed0ee145c341ac69f96
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2019-01-25 12:15:59 +0100

    fake in some sample data into the Gopass CLI bridge for recording a better intro-README-video

--- src/renderer/secrets/Gopass.ts
+++ src/renderer/secrets/Gopass.ts
@@ -7,8 +7,8 @@ export interface HistoryEntry {
     message: string
 }
 
-const lineSplitRegex = /\r?\n/
-const isDefined = (value: string) => !!value
+// const lineSplitRegex = /\r?\n/
+// const isDefined = (value: string) => !!value
 const escapeShellValue = (value: string) => value.replace(/(["'$`\\])/g, '\\$1')
 
 export default class Gopass {
@@ -18,28 +18,29 @@ export default class Gopass {
         return Gopass.execute(`show "${escapeShellValue(key)}" -c`)
     }
 
-    public static async show(key: string): Promise<string> {
-        return Gopass.execute(`show -f "${escapeShellValue(key)}"`)
+    public static show(key: string): Promise<string> {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(btoa(key))
+            }, 500)
+        })
     }
 
     public static async history(key: string): Promise<HistoryEntry[]> {
-        try {
-            return (await Gopass.execute(`history "${escapeShellValue(key)}"`))
-                .split(lineSplitRegex)
-                .filter(isDefined)
-                .map(historyLine => {
-                    const lineSplit = historyLine.split(' - ')
-
-                    return {
-                        hash: lineSplit[0],
-                        author: lineSplit[1],
-                        timestamp: lineSplit[2],
-                        message: lineSplit[3]
-                    }
-                })
-        } catch {
-            return []
-        }
+        return [
+            {
+                hash: 'asdf42s',
+                author: 'Matthias Rütten <matthias.ruetten@codecentric.de>',
+                timestamp: new Date() as any,
+                message: 'Removed Recipient 640F1D1D0E31A7BDEEEB8D99E8E300B26BE714EF'
+            },
+            {
+                hash: '512sfasdasd',
+                author: 'Jonas Verhoelen <jonas.verhoelen@codecentric.de>',
+                timestamp: new Date() as any,
+                message: 'Removed Recipient 640F1D1D0E31A7BDEEEB8D99E8E300B26BE714EF'
+            }
+        ]
     }
 
     public static async sync(): Promise<void> {
@@ -47,13 +48,40 @@ export default class Gopass {
     }
 
     public static async getAllSecretNames(): Promise<string[]> {
-        const flatSecrets = await Gopass.execute('list', ['--flat'])
-
-        return flatSecrets.split(lineSplitRegex).filter(isDefined)
+        return [
+            'codecentric/cassandra/url',
+            'codecentric/cassandra/username',
+            'codecentric/cassandra/password',
+            'codecentric/twenty-percent-time/some-stuff',
+            'codecentric/docker-registry/username',
+            'codecentric/docker-registry/password',
+            'codecentric/docker-registry/url',
+            'codecentric/keycloak/username',
+            'codecentric/keycloak/password',
+            'codecentric/keycloak/url',
+            'codecentric/kubernetes/dev/username',
+            'codecentric/kubernetes/dev/password',
+            'codecentric/kubernetes/dev/host',
+            'codecentric/kubernetes/prod/username',
+            'codecentric/kubernetes/prod/password',
+            'codecentric/aws/username',
+            'codecentric/aws/password',
+            'codecentric/opsgenie/username',
+            'codecentric/opsgenie/password',
+            'codecentric/slack/username',
+            'codecentric/slack/password',
+            'codecentric/kibana/username',
+            'codecentric/kibana/password',
+            'open-source/gopass-ui/some-secret',
+            'open-source/golumbus/some-secret',
+            'open-source/fish-history-to-zsh/some-secret',
+            'open-source/node-cassandra-migration/some-secret',
+            'open-source/react-mobx-i18n/some-secret'
+        ]
     }
 
-    public static async addSecret(name: string, value: string): Promise<void> {
-        await Gopass.execute('insert', [`"${escapeShellValue(name.trim())}"`], escapeShellValue(value))
+    public static addSecret(name: string, value: string): Promise<void> {
+        return Promise.resolve()
     }
 
     public static async editSecret(name: string, newValue: string): Promise<void> {
@@ -61,7 +89,7 @@ export default class Gopass {
     }
 
     public static async deleteSecret(name: string): Promise<void> {
-        await Gopass.execute('rm', ['--force', `"${escapeShellValue(name)}"`])
+        return Promise.resolve()
     }
 
     private static execute(command: string, args?: string[], pipeTextInto?: string): Promise<string> {
